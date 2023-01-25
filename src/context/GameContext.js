5a8b63cdc7b7f1cb31b95b49be075ab9975fd161
Alejae1998 <alejagr1998@gmail.com>
2023-01-25 12:06:35 -0800

    refactoring app.js, passing the state from the app to the context

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,8 +1,34 @@
 import { createContext, useState } from 'react';
+import initialCards from '../cards-data.js';
 
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   //states
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  return (
+    <GameContext.provider
+      value={{
+        deck,
+        setDeck,
+        selectedCard,
+        setSelectedCard,
+        playerTwoHand,
+        playerOneHand,
+        playerThreeHand,
+        to,
+        setTo,
+      }}
+    >
+      {children}
+    </GameContext.provider>
+  );
 };
 export { GameContext, GameProvider };
