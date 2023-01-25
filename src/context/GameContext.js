f29ec6fbf053c59b3b9cd9159a25892bb82642df
Alejae1998 <alejagr1998@gmail.com>
2023-01-25 11:38:36 -0800

    setting up the gamecontext added gameprovider to index.js

---
+++ src/context/GameContext.js
@@ -0,0 +1,8 @@
+import { createContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  //states
+};
+export { GameContext, GameProvider };
