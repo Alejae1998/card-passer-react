c35dfbca4547c08974d593e9da530afc768d75de
Alejae1998 <alejagr1998@gmail.com>
2023-01-25 12:15:15 -0800

    refactoring files one by one with useContext

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,28 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CardList from './CardList';
+import { GameContext } from '../context/GameContext.js';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setFrom,
-  setSelectedCard,
-  selectedCard,
-  to,
-}) {
+export default function Player({ player, hand }) {
+  const { setTo, to } = useContext(GameContext);
+
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
       onClick={() => setTo(player)}
     >
       <p>Player {player}</p>
-      <CardList
-        player={player}
-        cards={hand}
-        setFrom={setFrom}
-        selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard}
-      />
+      <CardList player={player} cards={hand} />
     </div>
   );
 }
