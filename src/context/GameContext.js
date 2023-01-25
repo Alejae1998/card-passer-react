e522c4d5d7a194715380871e15c9a05c5c0aa274
Alejae1998 <alejagr1998@gmail.com>
2023-01-25 12:27:07 -0800

    Provider wasn't capitalized so it was breaking everything :'(

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -13,8 +13,34 @@ const GameProvider = ({ children }) => {
   const [from, setFrom] = useState('deck');
   const [to, setTo] = useState(1);
 
+  function findCardIndex(value, suit, cards) {
+    return cards.findIndex((card) => card.value === value && card.suit === suit);
+  }
+
+  function passCard(card) {
+    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+
+    // arrays start at zero, but our players start at 1 :shrug:
+    const toHand = playerHands[to - 1] || deck;
+    const fromHand = playerHands[from - 1] || deck;
+
+    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+
+    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+
+    toHand.push(cardToMove);
+
+    toSetFunction([...toHand]);
+    fromSetFunction([...fromHand]);
+
+    setSelectedCard(null);
+  }
+
   return (
-    <GameContext.provider
+    <GameContext.Provider
       value={{
         deck,
         setDeck,
@@ -28,7 +54,7 @@ const GameProvider = ({ children }) => {
       }}
     >
       {children}
-    </GameContext.provider>
+    </GameContext.Provider>
   );
 };
 export { GameContext, GameProvider };
